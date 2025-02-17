3bfd0e0ca33b1a2ea910032674cfb357a6267b9d
Ewgeniy69 <git@github.com:Ewgeniy69/Helloworld.git>
2025-02-17 19:46:04 +0300

    Проведена работа над ошибками с учетом видеоразбора.

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,30 @@ public class Main {
         System.out.println("Task # 1");
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
 
         System.out.println("Task # 2");
 
         for (int i = 10; i > 0; i--) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
 
         System.out.println("Task # 3");
 
         for (int i = 0; i <= 17; i += 2) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
 
         System.out.println("Task # 4");
 
         for (int i = -10; i <= 10; i++) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
 
         System.out.println("Task # 5");
 
@@ -34,32 +38,35 @@ public class Main {
         System.out.println("Task # 6");
 
         for (int i = 7; i <= 98; i += 7) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
 
         System.out.println("Task # 7");
 
         for (int i = 1; i <= 512; i *=  2) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
+        System.out.println();
 
         System.out.println("Task # 8");
 
         int savings = 29000;
         int piggyBank = 0;
         for (int i = 1; i <= 12; i++) {
-            piggyBank = piggyBank + savings;
+            piggyBank += savings;
             System.out.println("Месяц " + i + " сумма накоплений равна " + piggyBank + " рублей");
         }
         System.out.println("Сумма годовых накоплений составит " + piggyBank + " рублей");
 
         System.out.println("Task # 9");
 
-        int investment = 0;
-        for (int i = 1; i <= 12; i++) {
-            investment = investment + investment / 100;
-            investment = investment + savings;
-            System.out.println("Месяц " + i + " сумма накоплений равна " + investment + " рублей");
+        piggyBank = 0;
+        double investment = 1D / 100;
+        for (int month = 1; month <= 12; month++) {
+            piggyBank += savings;
+            piggyBank = (int) (piggyBank * (1 + investment));
+            System.out.println("Месяц " + month + " сумма накоплений равна " + piggyBank + " рублей");
         }
 
         System.out.println("Task # 10");
